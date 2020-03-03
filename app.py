5a8c2c9bae6f49696ee2afc089bad2d8ada7208d
Jenny Pan <jenny.pan@ibm.com>
2020-03-03 17:32:26 -0500

    added site layout and basic pages

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route("/")
-def showMachineList():
-    return render_template('list.html')
+def landing():
+    return render_template('landing.html')
+@app.route("/about")
+def about():
+    return render_template('about.html')
+@app.route("/features")
+def features():
+    return render_template('features.html')
+# @app.route("/group-conversation.svg")
+# def groupconvo():
+#     return render_template('group-conversation.svg')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
